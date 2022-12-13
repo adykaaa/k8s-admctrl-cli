f39c08c9d4452ce535ad186404f7f5b5c2363148
adykaaa <gosupalanta@gmail.com>
2022-12-13 07:30:27 +0100

    getting all pods in a namespace

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	// Create a new Kubernetes config object
+	config, err := clientcmd.BuildConfigFromFlags("", "/home/adykaaa/.kube/config")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create a new Kubernetes clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Use the clientset to do something, such as list all of the pods in the current namespace
+	pods, err := clientset.CoreV1().Pods("kube-system").List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("There are %d pods in the current namespace\n", len(pods.Items))
 }
