9b9b0065ce3c847bc306274d56ac00272e199905
adykaaa <gosupalanta@gmail.com>
2022-12-14 13:00:05 +0100

    mutating webhook template

--- k8s/mutating_webhook.go
+++ k8s/mutating_webhook.go
@@ -1,1 +1,27 @@
 package k8s
+
+import (
+	"context"
+
+	"k8s.io/api/admissionregistration/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	//"k8s.io/client-go/kubernetes"
+	//"k8s.io/client-go/rest"
+)
+
+func deployMutatingWebhookConfiguration(namespace string) error {
+
+	// create the MutatingWebhookConfiguration object
+	mwc := &v1beta1.MutatingWebhookConfiguration{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "example-mutating-webhook-configuration",
+		},
+		Webhooks: []v1beta1.MutatingWebhook{
+			// add your webhooks here
+		},
+	}
+
+	// create the MutatingWebhookConfiguration
+	_, err = clientset.AdmissionregistrationV1beta1().MutatingWebhookConfigurations().Create(context.TODO(), mwc, metav1.CreateOptions{})
+	return err
+}
